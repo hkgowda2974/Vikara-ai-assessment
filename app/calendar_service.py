f3f8ffc62de2f4054b66e645ba4ea6bb87988948
hkgowda2974 <hkgowda2974@gmail.com>
2026-02-19 22:45:21 +0530

    load service account from environment variable

--- app/calendar_service.py
+++ app/calendar_service.py
@@ -1,14 +1,17 @@
+import os
+import json
 import datetime
 from dateutil import parser
 from dateutil import tz
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import os
-SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), "..", "service_account.json")
+
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+# Load credentials from environment variable
+service_account_info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info, scopes=SCOPES
 )
 
 service = build("calendar", "v3", credentials=credentials)
